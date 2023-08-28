d687ee53c1a0948f1a6903ed37b41386aef3dc58
xaviaxalee <davideffanga02@gmail.com>
2023-08-28 05:58:44 -0700

    0x05 - python exceptions project

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
+
+=============================
+6-raise_exception_msg.py
+
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
